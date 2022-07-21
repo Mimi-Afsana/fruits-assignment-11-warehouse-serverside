b0ea25118725425539278d00190dfe58f6429c2c
Mimi-Afsana <afsanamimi61220@gmail.com>
2022-07-21 08:59:52 +0800

    added

--- index.js
+++ index.js
@@ -31,81 +31,82 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-  async function run() {
-    try {
-      await client.connect();
-      const fruitsCollection = client.db("fruits1").collection("products");
-      app.post("/login", async (req, res) => {
-        const user = req.body;
-        console.log(user);
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: "1d",
-        });
-        res.send({ accessToken });
+async function run() {
+  try {
+    await client.connect();
+    const fruitsCollection = client.db("fruits1").collection("products");
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
       });
+      res.send({ accessToken });
+    });
 
-      app.get("/fruit", async (req, res) => {
-        const query = {};
+    app.get("/fruit", async (req, res) => {
+      const query = {};
+      const cursor = fruitsCollection.find(query);
+      const fruits = await cursor.toArray();
+      res.send(fruits);
+    });
+
+    // get particular fruit by id
+    app.get("/fruit/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const fruit = await fruitsCollection.findOne(query);
+      res.send(fruit);
+    });
+
+    app.put("/fruit/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateQuantity = req.body;
+      console.log(updateQuantity);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: updateQuantity.newQuantity,
+        },
+      };
+      const results = await fruitsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(results);
+    });
+    app.delete("/fruit/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruitsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // post
+    app.post("/fruit", async (req, res) => {
+      const newItem = req.body;
+      const result = await fruitsCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.get("/myItem", AuthorizatonJWt, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
         const cursor = fruitsCollection.find(query);
-        const fruits = await cursor.toArray();
-        res.send(fruits);
-      });
-
-      app.get("/fruit/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const fruit = await fruitsCollection.findOne(query);
-        res.send(fruit);
-      });
-
-      app.put("/fruit/:id", async (req, res) => {
-        const id = req.params.id;
-        const updateQuantity = req.body;
-        console.log(updateQuantity);
-        const filter = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updatedDoc = {
-          $set: {
-            quantity: updateQuantity.newQuantity,
-          },
-        };
-        const results = await fruitsCollection.updateOne(
-          filter,
-          updatedDoc,
-          options
-        );
-        res.send(results);
-      });
-      app.delete("/fruit/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await fruitsCollection.deleteOne(query);
-        res.send(result);
-      });
-
-      // post
-      app.post("/fruit", async (req, res) => {
-        const newItem = req.body;
-        const result = await fruitsCollection.insertOne(newItem);
-        res.send(result);
-      });
-
-      app.get("/myItem", AuthorizatonJWt, async (req, res) => {
-        const decodedEmail = req.decoded.email;
-        const email = req.query.email;
-        if (email === decodedEmail) {
-          const query = { email: email };
-          const cursor = fruitsCollection.find(query);
-          const orders = await cursor.toArray();
-          res.send(orders);
-        } else {
-          res.status(403).send({ message: "Forbidden access" });
-        }
-      });
-    } finally {
-    }
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "Forbidden access" });
+      }
+    });
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("fruits server warehouse");
